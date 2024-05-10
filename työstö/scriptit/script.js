00f213812d7d787b68d1e45f27dd941be7fc6ed9
kerony <rony.ahonen@edu.keuda.fi>
2024-05-10 23:43:31 +0300

    vahinko scriptin poisto

--- työstö/scriptit/script.js
+++ työstö/scriptit/script.js
@@ -11,29 +11,5 @@ function myFunction() {
   } else {
     navbar.classList.remove("sticky");
   }
-};
-
-function showContactForm() {
-  // Piilota kaikki lomakkeet
-  var forms = document.querySelectorAll('.contactForm');
-  forms.forEach(function(form) {
-      form.style.display = 'none';
-  });
-
-  // Näytä klikatun teoksen lomake
-  var clickedForm = event.target.closest('.aino_taide').querySelector('.contactForm');
-  clickedForm.style.display = 'block';
 }
 
-// Estä lomakkeen oletustoiminta ja toteuta oma toiminto
-document.querySelectorAll('.contactForm form').forEach(function(form) {
-  form.addEventListener('submit', function(event) {
-      event.preventDefault(); // Estä lomakkeen oletustoiminta
-      
-      // Toteuta haluamasi toiminto, esim. lomaketietojen käsittely
-      console.log('Lomaketiedot lähetetty');
-      
-      // Piilota lomake lähettämisen jälkeen
-      this.closest('.contactForm').style.display = 'none';
-  });
-});
